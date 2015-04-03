93f9ae83b7974a79878d0b5c171f111489f8c706
Yusuke Ohtsuka <tsuka0611@gmail.com>
2015-04-03 21:16:49 +0900

    add entry point.

---
+++ src/main/java/jp/co/aw/practice/jdbc/Main.java
@@ -0,0 +1,8 @@
+package jp.co.aw.practice.jdbc;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("fin");
+    }
+}
---
+++ src/test/java/jp/co/aw/practice/jdbc/MainTest.java
@@ -0,0 +1,5 @@
+package jp.co.aw.practice.jdbc;
+
+public class MainTest {
+
+}
