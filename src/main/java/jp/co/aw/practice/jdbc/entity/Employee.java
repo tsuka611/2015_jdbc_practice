51a61fc7676456c5470910b7c8ff3da8db081bd4
Yusuke Ohtsuka <tsuka0611@gmail.com>
2015-04-12 16:24:43 +0900

    create entity.

---
+++ src/main/java/jp/co/aw/practice/jdbc/entity/Employee.java
@@ -0,0 +1,36 @@
+package jp.co.aw.practice.jdbc.entity;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Employee {
+
+    @NonNull
+    Long id;
+
+    @NonNull
+    String name;
+
+    String mail;
+
+    String tel;
+
+    Timestamp updateDate;
+
+    @NonNull
+    Boolean isDeleted;
+
+    public Boolean setIsDeleted(Byte b) {
+        isDeleted = b == null || b.byteValue() != 0;
+        return isDeleted;
+    }
+}
