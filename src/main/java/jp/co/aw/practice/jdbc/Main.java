21bfbb2a9c9997be8cde58d3f7c7a5bfdf82bd53
Yusuke Ohtsuka <tsuka0611@gmail.com>
2015-04-13 02:10:47 +0900

    execute main logic.

--- src/main/java/jp/co/aw/practice/jdbc/Main.java
+++ src/main/java/jp/co/aw/practice/jdbc/Main.java
@@ -1,8 +1,31 @@
 package jp.co.aw.practice.jdbc;
 
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+import jp.co.aw.practice.jdbc.service.EmployeeService;
+import jp.co.aw.practice.jdbc.utils.ConsoleWrapper;
+
+import com.google.common.io.Closer;
+
 public class Main {
 
-    public static void main(String... args) {
-        System.out.println("fin");
+    public static void main(String... args) throws Exception {
+
+        Closer closer = Closer.create();
+        try {
+            Reader r = System.console() != null ? System.console().reader() : new InputStreamReader(System.in);
+            Writer w = System.console() != null ? System.console().writer() : new OutputStreamWriter(System.out);
+
+            ConsoleWrapper cw = closer.register(new ConsoleWrapper(r, w));
+            ConsoleExecutor exec = ConsoleExecutor.builder().employeeService(new EmployeeService()).build();
+            exec.execute(cw);
+        } catch (Exception e) {
+            throw closer.rethrow(e);
+        } finally {
+            closer.close();
+        }
     }
 }
